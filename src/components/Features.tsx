ba82562fc78ffaa21af10d0e71a8ad84184d36a9
Oms-343 <oms87473@gmail.com>
2024-09-06 17:53:52 +0530

    Features section animation complete

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,4 +1,7 @@
+"use client";
 import EcosystemIcon from "@/public/icons/ecosystem.svg";
+import { useEffect, useRef } from "react";
+import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
 
 const features = [
   {
@@ -18,6 +21,50 @@ const features = [
   },
 ];
 
+function Feature({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) {
+  const offsetX = useMotionValue(-100);
+  const offsetY = useMotionValue(-100);
+  const maskImage = useMotionTemplate`radial-gradient(100px 100px at ${offsetX}px ${offsetY}px,black,transparent)`;
+  const borderRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function updateMousePosition(e: MouseEvent) {
+      if (!borderRef.current) return;
+      const borderRect = borderRef.current?.getBoundingClientRect();
+      offsetX.set(e.x - borderRect.x);
+      offsetY.set(e.y - borderRect.y);
+    }
+    window.addEventListener("mousemove", updateMousePosition);
+
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+
+  return (
+    <div className="relative rounded-xl border border-white/30 px-5 py-10 text-center">
+      <motion.div
+        className="absolute inset-0 rounded-xl border-2 border-purple-400"
+        style={{
+          maskImage,
+        }}
+        ref={borderRef}
+      ></motion.div>
+      <div className="inline-flex h-14 w-14 items-center justify-center rounded-lg bg-white text-black">
+        <EcosystemIcon />
+      </div>
+      <h3 className="mb-2 mt-6 font-bold">{title}</h3>
+      <p className="text-white/70">{description}</p>
+    </div>
+  );
+}
+
 export default function Features() {
   return (
     <section className="bg-black py-[72px] text-white md:py-24">
@@ -35,16 +82,11 @@ export default function Features() {
 
         <div className="mt-16 flex flex-col gap-4 md:flex-1 md:flex-row">
           {features.map((feature, i) => (
-            <div
+            <Feature
               key={i}
-              className="rounded-xl border border-white/30 px-5 py-10 text-center"
-            >
-              <div className="inline-flex h-14 w-14 items-center justify-center rounded-lg bg-white text-black">
-                <EcosystemIcon />
-              </div>
-              <h3 className="mb-2 mt-6 font-bold">{feature.title}</h3>
-              <p className="text-white/70">{feature.description}</p>
-            </div>
+              title={feature.title}
+              description={feature.description}
+            />
           ))}
         </div>
       </div>
